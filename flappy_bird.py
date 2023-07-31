ed9d902ced14904c8e94bb96b2c203e6bc3a3e11
alebenedet <ale.benedetp@gmail.com>
2023-07-31 08:13:40 -0300

    class passaro

--- flappy_bird.py
+++ flappy_bird.py
@@ -68,10 +68,34 @@ class Passaro:
             if self.angulo > -90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
 
-    
+        def desenhar(self, tela):
+            # Animação do passaro
+            self.contagem_imagem += 1
+            
+            if self.contagem_imagem < self.TEMPO_ANIMACAO:
+                self.imagem = self.IMGS[0]
+            elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
+                self.imagem = self.IMGS[1]
+            elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+                self.imagem = self.IMGS[2]
+            elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+                self.imagem = self.IMGS[1]
+            elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+                self.imagem = self.IMGS[0]
+                self.contagem_imagem = 0
+            # Se o passaro estiver caindo, não bate as asas
+            if self.angulo <= -80:
+                self.imagem = self.IMGS[1]
+                self.contagem_imagem = self.TEMPO_ANIMACAO*2
 
+            # Desenha o passaro
+            imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+            pos_centro_imagem = self.imagem.get_rect(topleft = (self.x, self.y)).center
+            retangulo = imagem_rotacionada.get_rect(center=self.imagem.get_rect(center = pos_centro_imagem))
+            tela.blit(imagem_rotacionada, retangulo.topleft)
 
-    
+        def get_mask(self):
+            return pygame.mask.from_surface(self.imagem)    
 
 class Cano:
     pass
